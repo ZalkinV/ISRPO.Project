f4cc67c84f545fe2661b55c03e20b3ca462f9ad2
Zalkin <Zalkin.viktor@mail.ru>
2019-01-16 17:52:27 +0300

    In OnClick() add empty chart showing

--- ISRPO.Project/ChartShowing.cs
+++ ISRPO.Project/ChartShowing.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace ISRPO.Project
 {
@@ -10,7 +12,20 @@ namespace ISRPO.Project
 	{
 		public static void OnClick(object sender, EventArgs e)
 		{
+			Form chartForm = new Form
+			{
+				Text = "Work Chart",
+			};
 
+			Chart chart = new Chart
+			{
+				Parent = chartForm,
+				Dock = DockStyle.Fill,
+			};
+
+			chart.Titles.Add("Working Hours");
+
+			chartForm.Show();
 		}
 	}
 }
